bb6bec10a264299d0e09b5fe2b4101e5748ac357
Nadav Green <nadavgreen@Nadavs-MacBook-Pro.local>
2019-05-18 17:36:20 -0400

    first attempt at react native

---
+++ hash.js
@@ -0,0 +1,10 @@
+const mergeArray  = (arr1, arr2) => {
+	let length  = 0
+	if(arr1.length >= arr2.length) length = arr1.length
+	else length  = arr2.length
+	let temp = null
+	let mergeArr = []
+	for(let i = 0; i < length; i++){
+		if()
+	}
+}
