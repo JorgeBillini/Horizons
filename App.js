f41cf88f2389eec6c0658bf127a649e1c1f5450c
Jorge Billini <jorgebillini@Pursuits-MacBook-Air.local>
2019-05-29 19:01:25 -0400

    renamed containers to screens for best practices, created navigation folder

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Map } from './containers/Map'
+import Map  from './containers/Map'
 
 export default class App extends React.Component {
 
   	render() {
     		return <>
+			<Map />
     		</>
   	}
 }
