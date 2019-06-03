74bf9680a52725b6799a029eb37057616ff9a97f
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-06-03 00:01:00 -0400

    connected loggedin user to profile component

--- components/profile.js
+++ components/profile.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {View, Text} from 'react-native';
+import {AuthContext} from '../contexts/auth';
 
 export default class Profile extends Component{
     render (){
-        return <Text> User's Profile </Text>
+        return <AuthContext.Consumer>
+            {
+                user =>{
+                    return <View>
+                        <Text> User's Profile </Text>
+                        <Text> {user.id} </Text>
+                        <Text> {user.username} </Text>
+                        <Text> {user.email} </Text>
+                    </View>
+                }
+            }
+        </AuthContext.Consumer>
     }
 }
