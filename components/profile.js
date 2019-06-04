a336f15eb99c04d10ad0aecdc220a0a7056dd291
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-06-04 01:13:06 -0400

    added logout button to profile page

--- components/profile.js
+++ components/profile.js
@@ -1,20 +1,60 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, KeyboardAvoidingView, Text, TouchableOpacity, Image} from 'react-native';
 import {AuthContext} from '../contexts/auth';
+import firebase from '../firebase';
+import genericUserPic from '../assets/user.png';
 
 export default class Profile extends Component{
     render (){
         return <AuthContext.Consumer>
             {
                 user =>{
-                    return <View>
-                        <Text> User's Profile </Text>
-                        <Text> {user.id} </Text>
-                        <Text> {user.username} </Text>
-                        <Text> {user.email} </Text>
-                    </View>
+                    const profilePic = user.pic ? user.pic : genericUserPic;
+                    return (
+                        <KeyboardAvoidingView>
+                            
+                            <View style={styles.profile}>
+                                <Image source={profilePic} />
+                                <Text> {user.username} </Text>
+                            </View>
+                            
+                            <Text> {user.email} </Text>
+
+                            {/* logout button */}
+                            <TouchableOpacity 
+                                style={styles.logoutContainer}
+                                onPress={()=>{firebase.auth().signOut()}}
+                            >
+                                <Text style={styles.logoutText}>
+                                    Log Out
+                                </Text>
+                            </TouchableOpacity>
+
+                        </KeyboardAvoidingView>
+                    );
                 }
             }
         </AuthContext.Consumer>
     }
 }
+
+const styles = StyleSheet.create({
+    profile: {
+        borderColor: 'black',
+        borderWidth: 1,
+        
+    },
+    logoutContainer: {
+        backgroundColor: 'grey',
+        paddingVertical: 15,
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    logoutText: {
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: '700',
+        fontSize: 16
+    }
+});
