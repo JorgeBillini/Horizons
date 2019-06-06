8979294ce785b8b65c30f75eb3274d0c59b1c1ae
nadavgreen <43800418+nadavgreen@users.noreply.github.com>
2019-06-05 20:36:16 -0400

    Map (#17)
    * l
    
    * got axios call to get places
    
    * small change so I can open a PR
    
    * remove a photo that wasnt being used
    
    * changed region to intialregion in map.js
    
    * changed icons in map to forknife
    
    * created splash page and imported information for events

--- screens/Map.js
+++ screens/Map.js
@@ -1,118 +1,111 @@
 import React from 'react';
-import { Button, StyleSheet, View} from 'react-native';
-import { MapView, } from 'expo'
-import axios from 'axios'
+import { StyleSheet, View, Text, Image } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
+import axios from 'axios';
 
 export default class Map extends React.Component {
-	static navigationOptions= {header:null}
+	static navigationOptions = { header: null }
 
 	constructor(props){
 		super(props)
 		this.state = {
+			events: [],
+			places: [],
 			regionlatlong: {
-				latitude: 40.7127,
-				longitude: -74.0059,
+				latitude: 40.7217,
+				longitude: -74.0059, 
 			},
 			userlatlong: {
-				latitude: 40.7127,
+				latitude: 40.7217,
 				longitude: -74.0059,
 			},
-			events: [.00725, 2],
-			places: [{
-					business_name: '',
-					lat: 40.7127,
-					long: -74.0059
-				}]
 		}
-  }
-    	render() {
-        let {navigate} = this.props.navigation;
-    		return <>
+	}
 
+	render() {
+		return <>
 			<MapView
-				style={{ flex: 1 }}
-				provider={ MapView.PROVIDER_GOOGLE }
-				customMapStyle= { generatedMapStyle }
-				minZoomLevel = { 14.5 }
-				maxZoomLevel = { 20 }
-				intialRegion={{
-				latitude: this.state.regionlatlong.latitude,
-				longitude: this.state.regionlatlong.longitude,
-				latitudeDelta: 0.0922,
-          			longitudeDelta: 0.0421,
-				}}
-				showsUserLocation = {true}
-				showsMyLocationButton = {true}
-				onUserLocationChange = {() => {
-					this.getUserLatLong()
-					this.getPlaces()
-				}}
+					style = {{ flex: 1 }}
+					initialRegion = {{
+						latitude: this.state.regionlatlong.latitude,
+						longitude: this.state.regionlatlong.longitude,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+					}}
+					provider = { MapView.PROVIDER_GOOGLE }
+					customMapStyle = { generatedMapStyle }
+					minZoomLevel = { 14.5 }
+					maxZoomLevel = { 20 }
+					showsUserLocation = { true }
+					showsMyLocationButton = { true }
+					onUserLocationChange = {() => {
+						this.getUserLatLong()
+					}}
 			>
-			{
-				this.state.places.map((e, i)=>{
-					return <MapView.Marker
-						key={i}
-						coordinate={{latitude: parseFloat(e.lat),
-							longitude: parseFloat(e.long)}}
-						title={e.business_name}
-						description={`Restaurant`}
-					/>
-				})
-			}
+				{
+				this.state.places.map((e, i) => {
+					return <Marker
+						key = { i }
+						coordinate = {{ 
+							latitude: e.coordinates.latitude,
+							longitude: e.coordinates.longitude
+						}}
+						>
+						<Callout>
+							<View>
+								<Text>{e.name}</Text>
+							</View>
+						</Callout>
+						</Marker>})
+				}
 			</MapView>
-    		</>
-  	}
-	
-	componentDidMount(){
-		this.getRegionLatLong()
+		</>
+	}
+
+	componentDidMount() {
 		this.getUserLatLong()
-		this.getPlaces()
+	}
+	
+	getEvents = (latitude, longitude) => {
+		const 	min_lat = latitude - 0.00725
+			min_long = longitude - 0.00909
+			max_lat = latitude + 0.00725
+			max_long = longitude + 0.00909
+			url = `http://horizons-api.herokuapp.com/events`
+			term = `?min_lat=${min_lat}&max_lat=${max_lat}&min_long=${min_long}&max_long=${max_long}`
+		axios.get(`${url}${term}`)
+		.then(res=>console.log(JSON.stringify(res)))
+		.catch(_ => _)
 	}
 
-	getPlaces = () => {
-		const {latitude, longitude} = this.state.userlatlong
-		axios.get(`http://horizons-api.herokuapp.com/places/?lat=${latitude}&long=${longitude}`)
-		.then(data=>{
-			const {msg} = data.data
-			this.setState({places: msg})
+	getPlaces = (latitude, longitude, offset = 0) => {
+		const 	url = `https://api.yelp.com/v3/businesses/search`
+			term = `?latitude=${latitude}&longitude=${longitude}&limit=50&offset=${offset}&radius=600`
+			API_Key = `OO3Saz0hvxk-v0QFSvDyL79ElNRxg_BPX0A46BOqWVtdjYN_xRPa4vpvFuPwr6T-wZpzUNUM3uaL_FticZyIhVkKMwm3yFfDY_m7MQ-MxDI4lLQOeTDcaJjPwoXhXHYx`
+		axios.get(`${url}${term}`,
+			{headers: {Authorization: `Bearer ${API_Key}`}}
+		)
+		.then(res => {
+			const { businesses } = res.data
+			this.setState({ places: businesses })
 		})
-	}
-
-	con = () => {
-		console.log(JSON.stringify(this.state.places))
-	}
-		
-	onUserLocaChange = () => {
-		
+		.catch(_ => _)
 	}
 
 	getUserLatLong = () => {
 		navigator.geolocation.getCurrentPosition(location=>{
-				const {latitude, longitude} = location.coords
+			const 	{latitude, longitude} = location.coords
+				lat = this.state.userlatlong.latitude
+				long = this.state.userlatlong.longitude
+			if(lat <= (latitude - (.00725/2)) || lat >= (latitude + (.00725/2) ) || long <= (longitude - (.00909/2)) || long >= (longitude + (.00909/2))) {
 				this.setState({userlatlong: {latitude, longitude}})
+				this.getPlaces(latitude, longitude)
+				this.getEvents(latitude, longitude)
 			}
-		
-		)
-	}	
-
-	getRegionLatLong = () => {
-		navigator.geolocation.getCurrentPosition(location=>{
-				const {latitude, longitude} = location.coords
-				this.setState({regionlatlong: {latitude, longitude}})
-			}
-		)
+		})
 	}
-}
 
-const styles = StyleSheet.create({
-	button: {
-    		flex: 1,
-		position: 'absolute',
-		bottom: 5,
-		right: 2,
-		backgroundColor: '#000'
-  	},
-});
+}
 
 const generatedMapStyle =
 [
